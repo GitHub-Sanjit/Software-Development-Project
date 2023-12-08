5465a23c17575e66035018f25976c23960df4b90
Sanjit Sarkar <sanjit.est14@gmail.com>
2023-12-08 17:15:51 +0600

    Url updated

--- Week-4/PracticeTask/Module-15.5/musician/urls.py
+++ Week-4/PracticeTask/Module-15.5/musician/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.musician, name="musician"),
+    path("add/", views.musician, name="musician"),
     path("edit/<int:id>", views.edit_musician, name="edit_musician"),
 ]
