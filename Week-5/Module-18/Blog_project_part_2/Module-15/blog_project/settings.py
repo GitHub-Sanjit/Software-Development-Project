4e4afd3d0e54f82450af8dc0b4c1af6010f96d31
Sanjit Sarkar <sanjit.est14@gmail.com>
2023-12-11 14:10:07 +0600

    Module-18

--- Week-5/Module-18/Blog_project_part_2/Module-15/blog_project/settings.py
+++ Week-5/Module-18/Blog_project_part_2/Module-15/blog_project/settings.py
@@ -40,7 +40,6 @@ INSTALLED_APPS = [
     "author",
     "categories",
     "posts",
-    "profiles",
     "crispy_forms",
     "crispy_bootstrap5",
 ]
@@ -48,6 +47,8 @@ INSTALLED_APPS = [
 CRISPY_ALLOWED_TEMPLATE_PACKS = "bootstrap5"
 
 CRISPY_TEMPLATE_PACK = "bootstrap5"
+
+LOGIN_URL = '/author/login/'
 
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
@@ -126,6 +127,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = "static/"
+STATICFILES_DIRS = [
+    BASE_DIR / 'static',
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
