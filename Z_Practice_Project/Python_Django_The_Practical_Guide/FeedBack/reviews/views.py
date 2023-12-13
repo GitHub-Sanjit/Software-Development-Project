69d8058601ea3beed800ae17aebdb72f874a5ca4
Sanjit Sarkar <sanjit.est14@gmail.com>
2023-12-13 15:50:49 +0600

    Files Uploads

--- Z_Practice_Project/Python_Django_The_Practical_Guide/FeedBack/reviews/views.py
+++ Z_Practice_Project/Python_Django_The_Practical_Guide/FeedBack/reviews/views.py
@@ -1,6 +1,5 @@
-from typing import Any
-from django.db.models.query import QuerySet
-from django.http import HttpResponse, HttpResponseRedirect
+from reviews.models import Review
+from django.http import HttpResponseRedirect
 from django.shortcuts import render
 from django.views import View
 from django.views.generic.base import TemplateView
@@ -15,21 +14,21 @@ from .models import Review
 
 class ReviewView(CreateView):
     model = Review
-    fields = "__all__"
+    form_class = ReviewForm
     template_name = "reviews/review.html"
     success_url = "/thank-you"
 
 
 class ThankYouView(TemplateView):
-    template_name = 'reviews/thank_you.html'
+    template_name = "reviews/thank_you.html"
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        context['message'] = 'This works'
+        context["message"] = "This works!"
         return context
 
 
-class ReviewListView(ListView):
+class ReviewsListView(ListView):
     template_name = "reviews/review_list.html"
     model = Review
     context_object_name = "reviews"
