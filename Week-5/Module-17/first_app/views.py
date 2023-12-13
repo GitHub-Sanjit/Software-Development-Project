0f301666db9f3e741f34bb431aa9cb6db12cf77d
Sanjit Sarkar <sanjit.est14@gmail.com>
2023-12-13 11:40:12 +0600

    Module-19

--- Week-5/Module-17/first_app/views.py
+++ Week-5/Module-17/first_app/views.py
@@ -18,6 +18,7 @@ def signup(request):
                 messages.success(request, 'Account created successfully')
                 form.save()
                 print(form.cleaned_data)
+                return redirect('login')
         else:
             form = RegisterForm()
         return render(request, './signup.html', {'form': form})
@@ -26,20 +27,24 @@ def signup(request):
 
 
 def user_login(request):
-    if request.method == 'POST':
-        form = AuthenticationForm(request=request, data=request.POST)
-        if form.is_valid():
-            name = form.cleaned_data['username']
-            userpass = form.cleaned_data['password']
-            # check kortechi user database e ache kina
-            user = authenticate(username=name, password=userpass)
-            if user is not None:
-                login(request, user)
-                return redirect('profile')  # profile page e redirect 
+    if not request.user.is_authenticated:
+        if request.method == 'POST':
+            form = AuthenticationForm(request=request, data=request.POST)
+            if form.is_valid():
+                name = form.cleaned_data['username']
+                userpass = form.cleaned_data['password']
+                # check kortechi user database e ache kina
+                user = authenticate(username=name, password=userpass)
+                if user is not None:
+                    login(request, user)
+                    return redirect('profile')  # profile page e redirect
 
+        else:
+            form = AuthenticationForm()
+        return render(request, './login.html', {'form': form})
     else:
-        form = AuthenticationForm()
-    return render(request, './login.html', {'form': form})
+        messages.success(request, 'Already Signed In')
+        return redirect('profile')
 
 
 def profile(request):
