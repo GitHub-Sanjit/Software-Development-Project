7ec86e7e9360f4968bb8897f64da7854f22af1d3
Sanjit Sarkar <sanjit.est14@gmail.com>
2023-12-21 12:30:18 +0600

    Django Template Language

--- Z_Practice_Project/Python Django/jobapp/app/urls.py
+++ Z_Practice_Project/Python Django/jobapp/app/urls.py
@@ -6,6 +6,7 @@ from . import views
 
 urlpatterns = [
     path("", views.job_list, name="jobs_home"),
+    path("hello/", views.hello, name="hello"),
     path("job/<int:id>", views.job_detail, name="jobs_detail"),
     # path("job/<str:id>", views.job_detail),
 ]
