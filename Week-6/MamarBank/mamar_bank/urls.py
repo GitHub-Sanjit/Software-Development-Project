d97c8cbc857aa413f41cd30a4274017cdf88c420
Sanjit Sarkar <sanjit.est14@gmail.com>
2023-12-28 18:48:03 +0600

    Mamar Bank part 1 and 2

--- Week-6/MamarBank/mamar_bank/urls.py
+++ Week-6/MamarBank/mamar_bank/urls.py
@@ -1,5 +1,5 @@
 """
-URL configuration for jobapp project.
+URL configuration for mamar_bank project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/4.2/topics/http/urls/
@@ -15,11 +15,14 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path,include
-
-
+from django.urls import path, include
+from core.views import HomeView
 urlpatterns = [
+    path('', HomeView.as_view(), name='home'),
     path('admin/', admin.site.urls),
-    path("",include("app.urls")),
-    path("",include("subscribe.urls")),
+    path('accounts/', include('accounts.urls')),
+    path('transactions/', include('transactions.urls')),
 ]
+
+# accounts/login
+# accounts/register
