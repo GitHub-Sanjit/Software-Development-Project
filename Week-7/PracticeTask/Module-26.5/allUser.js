f01ff6260f355bdbac6569c30e594567265166fc
Sanjit Sarkar <sanjit.est14@gmail.com>
2024-01-08 19:28:20 +0600

    Smart Care REST API Service Model

---
+++ Week-7/PracticeTask/Module-26.5/allUser.js
@@ -0,0 +1,59 @@
+const loadUser = (value) => {
+  document.getElementById('alluser').innerHTML = '';
+  document.getElementById('spinner').style.display = 'block';
+  fetch(`https://fakestoreapi.com/users/${value ? value : ' '}`)
+    .then((res) => res.json())
+    .then((data) => {
+      console.log(data);
+      document.getElementById('spinner').style.display = 'none';
+      if (Array.isArray(data)) {
+        if (data?.length > 0) {
+          document.getElementById('nodata').style.display = 'none';
+          displayUsers(data);
+        } else {
+          document.getElementById('nodata').style.display = 'block';
+        }
+      } else {
+        displayUser(data);
+      }
+    })
+    .catch((err) => console.log(err));
+};
+const displayUsers = (users) => {
+  users?.forEach((user) => {
+    displayUser(user);
+  });
+};
+const displayUser = (user) => {
+  const parent = document.getElementById('alluser');
+  const div = document.createElement('div');
+  div.classList.add(
+    'col-lg-3',
+    'col-md-6',
+    'mb-4',
+    'mb-lg-0',
+    'card-container'
+  );
+  div.innerHTML = `
+    <div class="card rounded shadow-sm border-0">
+        <div class="card-body p-4">
+            <p class="small text-muted font-italic">ID: ${user?.id} </p>
+            <h5>${user?.name?.firstname} ${user?.name?.lastname} </h5>
+            <p>username: ${user?.username} </p>
+            <p>Password: ${user?.password} </p>
+            <p>phone: ${user?.phone} </p>
+            <p>Email: ${user?.email} </p>
+            <p>City: ${user?.address.city} </p>
+            <p>Zip code: ${user?.address?.zipcode} </p>
+            <p>Street: ${user?.address?.street} </p>
+        </div>
+    </div>
+    `;
+  parent.appendChild(div);
+};
+
+const handleSearch = () => {
+  const value = document.getElementById('search').value;
+  loadUser(value);
+};
+loadUser();
---
+++ Week-7/PracticeTask/Module-26.5/auth.js
@@ -0,0 +1,81 @@
+const createNewUser = (event) => {
+  event.preventDefault();
+  const newUserName = getValue('newUserName');
+  const newUserEmail = getValue('newUserEmail');
+  const newUserPassword = getValue('newUserPassword');
+  const newUserPhone = getValue('newUserPhone');
+  const newUserCity = getValue('newUserCity');
+  const newUserZipcode = getValue('newUserZipcode');
+  const newUserStreet = getValue('newUserStreet');
+
+  const newUser = {
+    name: {
+      firstname: newUserName,
+      lastname: '',
+    },
+    username: newUserName,
+    password: newUserPassword,
+    email: newUserEmail,
+    phone: newUserPhone,
+    address: {
+      city: newUserCity,
+      zipcode: newUserZipcode,
+      street: newUserStreet,
+    },
+  };
+  console.log(newUser);
+
+  fetch('https://fakestoreapi.com/users', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(newUser),
+  })
+    .then((response) => response.json())
+    .then((data) => {
+      console.log(data);
+      if (data.id) {
+        saveUserToLocalstorage(newUser);
+        window.location.href = 'alluser.html';
+      } else {
+        console.error('User creation failed');
+      }
+    })
+    .catch((error) => console.error('Error:', error));
+};
+const saveUserToLocalstorage = (user) => {
+  const existingUsers = JSON.parse(localStorage.getItem('users')) || [];
+  existingUsers.push(user);
+  localStorage.setItem('users', JSON.stringify(existingUsers));
+};
+const getValue = (id) => {
+  const value = document.getElementById(id).value;
+  return value;
+};
+
+const handleLogin = (event) => {
+  event.preventDefault();
+  const username = getValue('exampleInputEmail1');
+  const password = getValue('exampleInputPassword1');
+
+  if (username && password) {
+    fetch('https://fakestoreapi.com/auth/login', {
+      method: 'POST',
+      headers: { 'content-type': 'application/json' },
+      body: JSON.stringify({ username, password }),
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data);
+        if (data.success) {
+          window.location.href = 'index.html';
+        } else {
+          console.error('Login failed');
+        }
+      })
+      .catch((error) => console.error('Error:', error));
+  } else {
+    console.error('Username and password are required');
+  }
+};
